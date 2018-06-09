e0d95c2daa5f5ebcf7c871c8b0b31a4ab40e5626
Terrence Whaley <terrencew1995@gmail.com>
2018-06-09 13:33:56 -0500

    Fixing price bug.

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -43,6 +43,14 @@ public class MainActivity extends AppCompatActivity {
         displayPrice(quantity * 5);
     }
 
+    /**
+     * This method displays the given text on the screen.
+     */
+    private void displayMessage(String message) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(message);
+    }
+
 
     /**
      * This method displays the given quantity value on the screen.
